8c96be8b8c595b6795ebd38a4cb3d2273a251e5b
dudushy <eduardotalarico1337@gmail.com>
2023-12-30 00:27:53 -0300

    Recreating project on my desktop
    Something was wrong with the first project...I will update my laptop

---
+++ MauiAppDesktop/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppDesktop
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
