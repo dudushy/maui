7686b184ec8a1c0565d4ba08048f3cee2a0a1472
dudushy <eduardotalarico1337@gmail.com>
2025-06-25 18:40:40 -0300

    Create new MAUI Blazor project

---
+++ v2/MauiBlazorLantern/MauiBlazorLantern/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBlazorLantern
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ v2/MauiBlazorLantern/MauiBlazorLantern/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiBlazorLantern
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
